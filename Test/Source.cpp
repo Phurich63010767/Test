59158daad4c57cf16531a4c3d9a2fd53513113dd
Phurich63010767 <69179855+Phurich63010767@users.noreply.github.com>
2020-10-01 20:16:34 +0700

    Update Source.cpp

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -1,37 +1,106 @@
 #include<iostream>
+#include<conio.h>
+#include<Windows.h>
 using namespace std;
 
-void change(int i, int s, string x)
+void gotoxy(int x, int y)
 {
-    while (i < s)
-    {
-        char* p;
-        p = &x[i];
-        if (i % 2 == 0)
-        {
-            if (*p >= 97 && *p <= 122)
-            {
-                *p = *p - 32;
-            }
-        }
-        if (i % 2 != 0)
-        {
-            if (*p >= 65 && *p <= 90)
-            {
-                *p = *p + 32;
-            }
-        }
-        cout << *p;
-        i++;
-    }
+	COORD c = { x, y };
+	SetConsoleCursorPosition(
+		GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
+
+void draw_ship()
+{
+	cout << "YEEET";
+}
+
+void erase_ship(int x, int y)
+{
+	gotoxy(x, y);
+	cout << "     ";
+}
+
+void setcursor(bool visible)
+{
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO lpCursor;
+	lpCursor.bVisible = visible;
+	lpCursor.dwSize = 20;
+	SetConsoleCursorInfo(console, &lpCursor);
+}
+
+void setcolor(int fg, int bg)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
+}
+
+void draw_bullet(int x, int y)
+{
+	setcolor(2, 0);
+	gotoxy(x, y);
+	cout << "^";
+}
+void clear_bullet(int x, int y)
+{
+	setcolor(2, 0);
+	gotoxy(x, y);
+	cout << " ";
+}
+
 int main()
 {
-    int i = 0, s;
-    string x;
-    cin >> x;
-    s = x.size();     
-    change(i,s,x);
-
-    return 0;
+	char ch = '.';
+	int x = 38, y = 20;
+	int bx[5], by[5];
+	int i = 0;
+	int bullet = 0;
+	setcursor(0);
+	setcolor(2, 7);
+	gotoxy(x, y);
+	draw_ship();
+	do {
+		setcolor(2, 0);
+		if (_kbhit())
+		{
+			ch = _getch();
+			if ((ch == 97) && (x > 0))
+			{
+				erase_ship(x, y);
+				setcolor(2, 7);
+				gotoxy(--x, y);
+				draw_ship();
+			}
+			if ((ch == 100) && (x < 80))
+			{
+				erase_ship(x, y);
+				setcolor(2, 7);
+				gotoxy(++x, y);
+				draw_ship();
+			}
+			if ((ch == 32) && (bullet < 5))
+			{
+				bullet++;
+				bx[i] = x + 2;
+				by[i] = y - 1;
+			}
+			fflush(stdin);
+		}
+		if (bullet > 0 && bullet < 6)
+		{
+			clear_bullet(bx[i], by[i]);
+			if (by[i] == 0)
+			{
+				bullet--;
+			}
+			else
+			{
+				draw_bullet(bx[i], --by[i]);
+			}
+		}
+		Sleep(100);
+	} while (ch != 'x');
+	system("pause");
+	return 0;
 }
